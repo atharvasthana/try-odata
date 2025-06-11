b3b0cbb994f89d95d3ad71b0a076f838dc8fdcf8
atharvasthana <150160770+atharvasthana@users.noreply.github.com>
2025-06-11 14:13:01 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,30 +7,32 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)
 
-DB_PATH = 'output.db'
-DB_URL = 'https://drive.google.com/uc?export=download&id=1TKXgC9V8e9wuA_88uRmWL9kYuag85S1L'
+# File info
+DB_FILE = "output.db"
+GOOGLE_DRIVE_URL = "https://drive.google.com/uc?export=download&id=1TKXgC9V8e9wuA_88uRmWL9kYuag85S1L"
 
-
-# Download .db from Google Drive if not exists
-if not os.path.exists(DB_PATH):
-    print("📥 Downloading .db from Google Drive...")
-    r = requests.get(DB_URL)
-    with open(DB_PATH, 'wb') as f:
+# Download DB from Google Drive if not already present
+if not os.path.exists(DB_FILE):
+    print("Downloading database from Google Drive...")
+    r = requests.get(GOOGLE_DRIVE_URL)
+    with open(DB_FILE, 'wb') as f:
         f.write(r.content)
     print("✅ Download complete.")
 
+# Database connection
 def get_db_connection():
-    conn = sqlite3.connect(DB_PATH)
+    conn = sqlite3.connect(DB_FILE)
     conn.row_factory = sqlite3.Row
     return conn
 
+# OData $metadata endpoint
 @app.route('/odata/$metadata')
 def metadata():
     xml = '''<?xml version="1.0" encoding="utf-8"?>
-<edmx:Edmx Version="4.0" xmlns:edmx="http://docs.oasis-open.org/odata/ns/edmx">
+<edmx:Edmx xmlns:edmx="http://docs.oasis-open.org/odata/ns/edmx" Version="4.0">
   <edmx:DataServices>
-    <Schema Namespace="IsbnModel" xmlns="http://docs.oasis-open.org/odata/ns/edm">
-      <EntityType Name="importbook0_1">
+    <Schema xmlns="http://docs.oasis-open.org/odata/ns/edm" Namespace="IsbnModel">
+      <EntityType Name="importbook">
         <Key><PropertyRef Name="id"/></Key>
         <Property Name="id" Type="Edm.Int32" Nullable="false"/>
         <Property Name="COL 1" Type="Edm.String"/>
@@ -43,28 +45,33 @@ def metadata():
         <Property Name="COL 8" Type="Edm.String"/>
       </EntityType>
       <EntityContainer Name="Container">
-        <EntitySet Name="importbook0_1" EntityType="IsbnModel.importbook0_1"/>
+        <EntitySet Name="importbook" EntityType="IsbnModel.importbook"/>
       </EntityContainer>
     </Schema>
   </edmx:DataServices>
 </edmx:Edmx>'''
     return Response(xml, mimetype='application/xml')
 
-@app.route('/odata/importbook0_1', methods=['GET'])
+# Main OData entity endpoint
+@app.route('/odata/importbook', methods=['GET'])
 def get_isbn_data():
-    top = int(request.args.get('$top', 100))
+    limit = int(request.args.get('$top', 100))
     conn = get_db_connection()
-    rows = conn.execute('SELECT * FROM importbook0_1 LIMIT ?', (top,)).fetchall()
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM importbook LIMIT ?", (limit,))
+    rows = cursor.fetchall()
     conn.close()
-    data = [dict(r) for r in rows]
+
+    result = [dict(row) for row in rows]
+
     return jsonify({
-        "@odata.context": request.url_root.rstrip('/') + "/odata/$metadata#importbook0_1",
-        "value": data
+        "@odata.context": request.url_root.rstrip('/') + "/odata/$metadata#importbook",
+        "value": result
     })
 
 @app.route('/')
 def home():
-    return "✅ OData API is running and using .db from Google Drive!"
+    return "✅ OData API is running and serving data from Google Drive!"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=10000)
