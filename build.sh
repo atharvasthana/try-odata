9fe9127e5a58688e2a8cc491a8e2064679f406c8
atharvasthana <150160770+atharvasthana@users.noreply.github.com>
2025-06-12 15:46:31 +0530

    Create build.sh

---
+++ build.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+echo "🔽 Downloading output.db from Google Drive..."
+curl -L -o output.db "https://drive.google.com/uc?export=download&id=1TKXgC9V8e9wuA_88uRmWL9kYuag85S1L"
+echo "✅ Done downloading DB."
