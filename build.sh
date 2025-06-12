d27874eac3387d50b4e2ce2b361d199f542b5260
atharvasthana <150160770+atharvasthana@users.noreply.github.com>
2025-06-12 22:29:09 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 echo "🔽 Downloading data.sqlite from Google Drive..."
-curl -L -o output.db "https://drive.google.com/file/d/1zf1Otx3Lvfednl1suK7DEZYE8ZCN2YDv/view?usp=sharing"
+curl -L -o data.sqlite "https://drive.google.com/file/d/1zf1Otx3Lvfednl1suK7DEZYE8ZCN2YDv/view?usp=sharing"
 echo "✅ Done downloading DB."
