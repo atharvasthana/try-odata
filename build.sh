918b1f5db19a0501394d6665bab18faa0820d3e5
atharvasthana <150160770+atharvasthana@users.noreply.github.com>
2025-06-15 11:52:13 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,9 +1,9 @@
-#!/bin/bash
+#!/bin/bash    
 
 echo "⬇ Downloading books.json from Google Drive..."
 
-FILE_ID="15qWEjGpHmzVWzIaQwy8AgUOqRAV38kxr"
-OUTPUT_FILE="books.json"
+FILE_ID="1sE1K-wb9h6XVMXqC55oVhLkKUGNOuISf"
+OUTPUT_FILE="datanew.json"
 
 curl -L -o "$OUTPUT_FILE" "https://drive.google.com/uc?export=download&id=${FILE_ID}"
 
